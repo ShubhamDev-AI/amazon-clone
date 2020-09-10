613cd171f2762cb03cd3fb885b0cd9fa9abbe5f4
shubham zade <67915582+ShubhamDev-AI@users.noreply.github.com>
2020-09-10 20:50:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Header from './Header';
+import Home from './Home';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/*home  */}
+      <Home />
+     
+    </div>
+  );
+}
